a615df232bc6eefa2e5d066a92eebf3cb8bd81ba
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-02-01 13:52:37 +0100

    Initial commit of Spring

---
+++ src/main/kotlin/no/nav/arbeid/pam/pamcvbrukergenerator/PamCvBrukergeneratorApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.arbeid.pam.pamcvbrukergenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PamCvBrukergeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<PamCvBrukergeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/arbeid/pam/pamcvbrukergenerator/PamCvBrukergeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.arbeid.pam.pamcvbrukergenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PamCvBrukergeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
