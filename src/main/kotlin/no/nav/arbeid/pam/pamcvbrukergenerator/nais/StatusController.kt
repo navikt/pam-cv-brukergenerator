05a0d498e7200f0668890a19e8e1aa97175b2634
Magnus S Espeland <magnus.s.espeland@nav.no>
2021-02-04 09:43:40 +0100

    Implement isAlive and isReady Co-authored-by: marthehagen <marthe.hagen@nav.no>

---
+++ src/main/kotlin/no/nav/arbeid/pam/pamcvbrukergenerator/nais/StatusController.kt
@@ -0,0 +1,16 @@
+package no.nav.arbeid.pam.pamcvbrukergenerator.nais
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/internal")
+class StatusController {
+
+    @GetMapping("isAlive")
+    fun isAlive() = "OK"
+
+    @GetMapping("isReady")
+    fun isReady() = "OK"
+}
